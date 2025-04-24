ffb477b950d242bbfbcf43462d7aa0a804b333dd
Nayan <dreamerboynayan@gmail.com>
2025-04-24 19:25:04 +0600

    Creation Using Object Instance

--- index.js
+++ index.js
@@ -2,15 +2,30 @@
 
 // Creation Using Object Literal
 
-let person = {
-    name: "Nayan",
-    age: 22,
-    city: "Faridpur",
-    isStudent: true,
+// let person = {
+//     name: "Nayan",
+//     age: 22,
+//     city: "Faridpur",
+//     isStudent: true,
 
-    getInfo: function(){
-        return `My name is ${person.name}.`
-    }
+//     getInfo: function(){
+//         return `My name is ${person.name}.`
+//     }
+// }
+
+// console.log(person.getInfo());
+
+// Creation Using Object Instance 
+
+const person = new Object()
+
+person.name = "Nayan"
+person.age = 22
+person.city = "Faridpur"
+person.isStudent = true
+person.getInfo = () => {
+    return `My name is ${person.name}. I'm ${person.age} years old. I live in ${person.city}.`
 }
 
+console.log(person);
 console.log(person.getInfo());
